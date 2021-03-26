a9bfe4e8c0ebcf08cdadaaf5a62ab48f452edaf8
sskvortsov <sskvortsov@gear-games.com>
2021-03-26 12:24:32 +0300

    learning branch

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Input.GetAxis("Jump");
+        var velocity = rb.velocity;
+        velocity = new Vector3(input, velocity.y, velocity.z);
+        rb.velocity = velocity;
+    }
+}
---
+++ Assets/Scripts/Starter.cs
@@ -0,0 +1,64 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Starter : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        //If we already connected then return
+        if (PhotonNetwork.NetworkingClient.LoadBalancingPeer.PeerState != PeerStateValue.Disconnected)
+            return;
+
+        //Set settings
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "0.0.1";
+
+        //Connect
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    #region Handling fails
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        ReportFail(message);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        ReportFail(message);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        ReportFail(message);
+    }
+
+    void ReportFail(string message)
+    {
+        Debug.Log(message);
+    }
+
+    #endregion
+
+    public override void OnConnectedToMaster()
+    {
+        var test = new OpJoinRandomRoomParams();
+        PhotonNetwork.NetworkingClient.OpJoinRandomOrCreateRoom(null, null);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("OnCreatedRoom");
+        PhotonNetwork.LoadLevel("Gameplay");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+        PhotonNetwork.LoadLevel("Gameplay");
+    }
+}
