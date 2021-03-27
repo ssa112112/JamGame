9418b7b3df73895018c6745850ed01cef52e3c06
sskvortsov <sskvortsov@gear-games.com>
2021-03-27 16:31:37 +0300

    Change live sync

--- Assets/amakarov/Scripts/ChangeLifesUp.cs
+++ Assets/amakarov/Scripts/ChangeLifesUp.cs
@@ -1,13 +1,19 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using sskvortsov.Scripts.GamePlay;
+using UnityEngine;
 
 public class ChangeLifesUp : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.CompareTag("Player"))
+        if (PhotonNetwork.IsMasterClient)
         {
-            GameManager.Instance.IncreaseLives();
-            Destroy(gameObject);
+            if (other.transform.CompareTag("Player"))
+            {
+                GameManager.Instance.IncreaseLives();
+                Destroy(gameObject);
+                NetworkEventManager.SendChangeLiveEvent(1);
+            }
         }
     }
 }
